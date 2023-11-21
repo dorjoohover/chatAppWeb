b5fd4bc5f4a9b67a970019d7556aea64512461dd
dagina <aaban9494@gmail.com>
2023-11-22 06:08:14 +0800

    asdf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,9 @@ import styles from "./page.module.css";
 import Intro from "../components/Intro";
 import Description from "../components/Description";
 
-import LocomotiveScroll from "locomotive-scroll";
 import { Box } from "@chakra-ui/react";
 import About from "@/components/About";
 export default function Home() {
-  useEffect(() => {
-    (async () => {
-      const locomotiveScroll = new LocomotiveScroll();
-    })();
-  }, []);
-
   return (
     <Box>
       <Intro />
